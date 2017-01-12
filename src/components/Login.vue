9da9e34554b7199eefaa8e56bebaae686b6f4521
alexseth1124 <alexseth1124@gmail.com>
2017-01-12 17:35:38 -0500

    sets up login page

---
+++ src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="col-sm-4 col-sm-offset-4 login-form">
+    <h2 v-if="!isNewPassword">Level-Up Your Community</h2>
+    <h2 v-else>Please enter new password</h2>
+    <p>Connecting non-profits with AWS certified professionals across the globe.</p>
+    <button>Mentor Sign In</button>
+    <button>Non-Profit Sign In</button>
+
+    <p>Not a member? Register now!</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import VueRouter from 'vue-router'
+import Resource from 'vue-resource'
+import routes from './routes'
+import store from './store'
+
+Vue.use(VueRouter)
+Vue.use(Resource)
+
+var router = new VueRouter({
+  routes: routes,
+  mode: 'history',
+  scrollBehavior: function (to, from, savedPosition) {
+    return savedPosition || { x: 0, y: 0 }
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+  el: '#root',
+  router: router,
+  store: store
 })
