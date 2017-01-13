2f7c1641bcb4975dbfbaf5cf580f934fd58052ca
alexmartin1124 <alexseth1124@gmail.com>
2017-01-13 13:17:50 -0500

    adds background image

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="col-sm-4 col-sm-offset-4 login-form">
-    <h1>Level-Up Your Community</h1>
-    <h3>Connecting non-profits with AWS certified professionals across the globe.</h3>
-    <button>Mentor Sign In</button>
-    <button>Non-Profit Sign In</button>
+  <div>
+  <div class="container" id="login-page">
+    <img id="login-background" src="/static/images/login-background.jpg">
+    <div class="row">
+    <header class="header">
+      <div class=".col-md-6">
+      <img class="logo" id="acg-logo" src="/static/images/acg-logo.png">
+      </div>
+      <div class=".col-md-6">
+      <img class="logo" id="aws-logo" src="/static/images/aws-logo.png">
+      </div>
+    </header>
+    </div>
+    <section class="content">
+      <div class="body" id="login-body">
+        <div class="row">
+        <h1>Level-Up <span id="your">Your</span> Community</h1>
+        </div>
+        <div class="row">
+        <h4>Connecting non-profits with AWS certified professionals across the globe.</h4>
+      </div>
+        <button class="btn btn-login">MENTOR SIGN IN</button>
+        <button class="btn btn-login">NON-PROFIT SIGN IN</button>
 
-    <p>Not a member? Register now!</p>
+        <p>Not a member? Register now!</p>
+      </div>
+    </section>
+    </div>
   </div>
 </template>
 
